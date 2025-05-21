b883ee0641d53896dcf23d49ccb870f093874cee
Arora962 <157892517+Arora962@users.noreply.github.com>
2025-05-21 12:10:41 +0530

    Initial commit: Add TypeScript setup and challenge1

---
+++ challenge1.ts
@@ -0,0 +1,11 @@
+let message: string = "Hello, my name is Kriti!";
+console.log(message);
+ 
+let age: number = 19;
+console.log("I am " + age + " years old.");
+
+/* Uncommenting the following lines will cause a type error
+//because we are trying to assign a number to a string variable
+let myString: string;
+myString = 25;
+console.log(myString);*/
